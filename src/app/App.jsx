16425f946f2acc085d483e7af6e00a24a293c5cb
3belpolin <3belpolin@gmail.com>
2025-05-15 08:31:17 +0300

    feat: did hw-6

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { CartProvider } from '@context/CartContext';
-import {HomePage} from "@pages/homePage/HomePage";
 import { Layout } from "@ui/layout/Layout.jsx";
+import {AppRoutes} from "@routes/appRoutes";
+import {BrowserRouter} from "react-router-dom";
+import { AuthProvider } from "@context/AuthContext";
 
 const App = () => {
     return (
-      <CartProvider>
-        <Layout>
-            <HomePage/>
-        </Layout>
-      </CartProvider>
+        <BrowserRouter>
+            <AuthProvider>
+            <CartProvider>
+                <Layout>
+                    <AppRoutes />
+                </Layout>
+            </CartProvider>
+            </AuthProvider>
+        </BrowserRouter>
     );
   }
 
