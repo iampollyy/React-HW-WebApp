ff0bb0d2d0a8242827cc33d73692a3e891211571
3belpolin <3belpolin@gmail.com>
2025-06-20 14:09:04 +0300

    feat: dark-theme realized

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -5,3 +5,4 @@ export {default as Input} from './input/Input';
 export {default as LoginForm} from './forms/loginForm/LoginForm';
 export { default as ButtonPattern } from './button/ButtonPattern';
 export { default as OrderForm } from './forms/orderForm/OrderForm';
+export { default as DarkMode } from './darkMode/DarkMode';
--- src/shared/ui/socialMedia/SocialMedia.tsx
+++ src/shared/ui/socialMedia/SocialMedia.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import styles from "./socialMedia.module.scss";
 
-import Insta from "@images/icons/inst.svg";
-import X from "@images/icons/x.svg";
-import YouTube from "@images/icons/youTube.svg";
 
-const SocialMedia: React.FC = () =>{
-    return (
-      <div className={styles.social__media}>
-        <div className={styles.circle}><a href="https://www.instagram.com/iampollyaa?igsh=MTF2NnI0cGtpZ2FreA%3D%3D&utm_source=qr" target='_blank'><img src={Insta} alt="Instagram" /></a></div>
-        <div className={styles.circle}><a href="https://x.com/" target='_blank'><img src={X} alt="Twitter" /></a></div>
-        <div className={styles.circle}><a href="https://www.youtube.com/" target='_blank'><img src={YouTube} alt="YouTube" /></a></div>
+const SocialMedia: React.FC = () => {
+  return (
+    <div className={styles.social__media}>
+      <div className={styles.circle}>
+        <a href="https://www.instagram.com/iampollyaa?igsh=..." target="_blank" rel="noreferrer">
+          <span className={`${styles.icon} ${styles.insta}`} />
+        </a>
       </div>
-    )
-}
+      <div className={styles.circle}>
+        <a href="https://x.com/" target="_blank" rel="noreferrer">
+          <span className={`${styles.icon} ${styles.x}`} />
+        </a>
+      </div>
+      <div className={styles.circle}>
+        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+          <span className={`${styles.icon} ${styles.youtube}`} />
+        </a>
+      </div>
+    </div>
+  );
+};
+
 export default SocialMedia;
